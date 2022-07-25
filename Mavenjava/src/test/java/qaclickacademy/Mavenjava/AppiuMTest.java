83e9431aec689c16ba628c3ffdff9a0bb533647b
Jovan K <jovan.kovacevic@hotmail.com>
2022-07-25 13:50:51 +0200

    First commint JK'

---
+++ Mavenjava/src/test/java/qaclickacademy/Mavenjava/AppiuMTest.java
@@ -0,0 +1,28 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiuMTest {
+
+	
+	@Test
+	public void NativeAppAndorid()
+	{
+		
+		System.out.println("NativeAppAndorid");
+		
+	}
+	
+	
+	@Test
+	public void IOSApps()
+	{
+		
+		System.out.println("IOSApps");
+		
+	}
+	
+	
+	
+	
+}
---
+++ Mavenjava/src/test/java/qaclickacademy/Mavenjava/SeleniuMTest.java
@@ -0,0 +1,26 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniuMTest {
+
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		
+		System.out.println("BrowserAutomation");
+		
+	}
+	
+	
+	@Test
+	public void elementsUI()
+	{
+		
+		System.out.println("elementsUI");
+		
+	}
+	
+	
+}
